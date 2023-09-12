be00c12dfb1b3400f1b7ddc37a07838ebe51b38e
Anastasiia Podopryhora <anastasia95podoprigora@gmail.com>
2023-09-12 21:14:53 +0200

    Completed lab

--- index.js
+++ index.js
@@ -17,7 +17,41 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+    Recipe.create(myRecipe)
+    .then(recipe => console.log('The recipe is saved and its value is: ', recipe))
+    .catch(error => console.log('An error happened while saving a new recipe:', error));
+}).then(()=>{Recipe.insertMany(data)
+  .then(recipe => console.log('The recipe is saved and its value is: ', recipe))
+  .then(()=>Recipe.findOneAndUpdate({ "title":"Rigatoni alla Genovese" }, { "duration": 100 }))
+  .then(recipe => console.log('The recipe is updated and its value is: ', recipe))
+  .then(()=>Recipe.deleteOne({"title":"Carrot Cake"}))
+  .then(recipe => console.log('The recipe was deleted and its value is: ', recipe))
+  
+  .catch(error => console.log('An error happened while saving a new recipe:', error))
+})
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
   });
+});
+
+const myRecipe = [{
+  "title": "Caesar Salad",
+  "level": "Amateur Chef",
+  "ingredients": [
+  "1 large or 2 small heads of romaine lettuce",
+  "Parmesan cheese, shredded or shaved",
+  "Crisp croutons - homemade can be made several days ahead",
+  "Caesar salad dressing",
+ ],
+"cuisine": "Mexico",
+"dishType": "other",
+"image": "https://img.chefkoch-cdn.de/rezepte/956701201250684/bilder/1512860/crop-960x540/caesar-salad.jpg",
+"duration": 40,
+"creator": "Caesar Cardini"}]
+
+
+
+
